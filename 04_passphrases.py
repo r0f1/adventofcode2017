4e73bbb473f8a62963ffacd93cdf8ebebc3b4b25
Florian Rohrer <semicolonator@gmail.com>
2017-12-04 22:17:01 +0000

    typo

--- 04_passphrases.py
+++ 04_passphrases.py
@@ -3,11 +3,11 @@ count2 = 0
 with open("04_input.txt", "r") as f:
 	for l in f:
 		chunks = l.split()
-		l = len(chunks)
-		if l == len(set(chunks)):
+		r = len(chunks)
+		if r == len(set(chunks)):
 			count += 1
 			s = set("".join(sorted(c)) for c in chunks)
-			if l == len(s):
+			if r == len(s):
 				count2 += 1
 print(count)
 print(count2)
