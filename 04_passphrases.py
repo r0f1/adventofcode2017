2eec9f10822f2d0e6e8c93216a44e57df1724920
Florian Rohrer <semicolonator@gmail.com>
2017-12-04 22:15:30 +0000

    day 4

--- 04_passphrases.py
+++ 04_passphrases.py
@@ -1,13 +1,13 @@
 count = 0
 count2 = 0
-with open("04_input.txt") as f:
+with open("04_input.txt", "r") as f:
 	for l in f:
 		chunks = l.split()
-		r = len(chunks)
-		if r == len(set(chunks)):
+		l = len(chunks)
+		if l == len(set(chunks)):
 			count += 1
 			s = set("".join(sorted(c)) for c in chunks)
-			if r == len(s):
+			if l == len(s):
 				count2 += 1
 print(count)
 print(count2)
