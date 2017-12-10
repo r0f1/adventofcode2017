d885fbfa032c0cfcce2fd21962267d960d2e725d
Florian Rohrer <semicolonator@gmail.com>
2017-12-10 11:41:25 +0100

    asdf

---
+++ 10_hashing.py
@@ -0,0 +1,26 @@
+def reversed_part(m, curr, p):
+    end = p + curr
+    if end >= len(m):
+        n = m[curr:] + m[:end-len(m)]
+    else:
+        n = m[curr:end]    
+    n.reverse()
+    return n
+
+def alter_list(m, n, curr):
+    for e in n:
+        m[curr] = e
+        curr = (curr + 1) % len(m)
+
+puzzle_input = [31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33]
+m = list(range(256))
+curr = 0
+skip = 0
+
+for p in puzzle_input:
+    n = reversed_part(m, curr, p)
+    alter_list(m, n, curr)
+    curr = (curr + p + skip) % len(m)
+    skip += 1
+
+print(m[0]*m[1]) # Part One
